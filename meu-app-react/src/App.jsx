ba675bbff6e96528d498962e40abcd700209307f
01Dashh <gablucas12@gmail.com>
2026-06-16 20:29:01 -0300

    refactor: remove css padrao e estrutura roteamento

--- meu-app-react/src/App.jsx
+++ meu-app-react/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
 import Menu from './components/Menu';
 import Inicio from './pages/Inicio';
 import Cadastro from './pages/Cadastro';
@@ -6,17 +7,17 @@ import Listagem from './pages/Listagem';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      {/* O Menu fica fora das Routes para aparecer em todas as páginas */}
-      <Menu /> 
-      
-      <main style={{ padding: '0 10px' }}>
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/listagem" element={<Listagem />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Menu /> 
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/listagem" element={<Listagem />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
